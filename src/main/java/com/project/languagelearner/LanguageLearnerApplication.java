3e5cd9118851ad3c37c40e926f4891709101cb3a
JustinJefferson <justleland@gmail.com>
2019-09-23 13:00:24 -0400

    Starting project

---
+++ src/main/java/com/project/languagelearner/LanguageLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.project.languagelearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearnerApplication.class, args);
+	}
+
+}
